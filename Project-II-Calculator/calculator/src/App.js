c6561ff9a210114099653a2737eb6b0bdbcaf42f
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2019-06-01 19:12:34 -0500

    calculator commit

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ButtonContainer from './components/ButtonComponents/ButtonContainer';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
-const App = () => {
-  return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    return (
+      <div className="calContainer">
+        <CalculatorDisplay />
+        <ButtonContainer>
+          <ActionButton className="bigText" text="clear" />
+          <NumberButton bgColor="actionBut" text="/" />
+        </ButtonContainer>
+        <ButtonContainer>
+          <NumberButton bgColor="numBut" text="7" />
+          <NumberButton bgColor="numBut" text="8" />
+          <NumberButton bgColor="numBut" text="9" />
+          <NumberButton bgColor="actionBut" text="X" />
+        </ButtonContainer>
+        <ButtonContainer>
+          <NumberButton bgColor="numBut" text="4" />
+          <NumberButton bgColor="numBut" text="5" />
+          <NumberButton bgColor="numBut" text="6" />
+          <NumberButton bgColor="actionBut" text="-" />
+        </ButtonContainer>
+        <ButtonContainer>
+          <NumberButton bgColor="numBut" text="1" />
+          <NumberButton bgColor="numBut" text="2" />
+          <NumberButton bgColor="numBut" text="3" />
+          <NumberButton bgColor="actionBut" text="+" />
+        </ButtonContainer>
+        <ButtonContainer>
+          <ActionButton className="bigText" text="0" />
+          <NumberButton bgColor="actionBut" text="=" />
+        </ButtonContainer>
+      </div>
+    );
+  }
+}
 
 export default App;
